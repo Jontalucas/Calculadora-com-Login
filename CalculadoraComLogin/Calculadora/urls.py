0871765c511c820dc9a1f0b62624ea850871ff00
Jontalucas <99298697+Jontalucas@users.noreply.github.com>
2025-07-28 05:32:25 -0300

    Delete CalculadoraComLogin directory

--- CalculadoraComLogin/Calculadora/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'calculadora'
-urlpatterns = [
-    path('', views.Calculadora, name = 'Calculadora'),
-    path('logout', views.Logout, name = 'Logout'),
-]
